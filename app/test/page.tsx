a0e358ea264b2cd91087a432fe14af52aa3b4009
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2025-10-30 21:12:39 +0700

    minor upadate

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { DivFlexColumn } from "@/components/LayoutDiv/LayoutDiv";
+import TextField from "@/components/TextInput/TextField";
 import ModalExample from "@/hooks/useModal.example";
 
 
@@ -8,7 +9,15 @@ export default function Test() {
 
     return (
         <DivFlexColumn style={{ margin: 'var(--PAGE-Prop-Body-margin)', gap: 'var(--Spacing-Spacing-M)' }}>
-            <ModalExample />
+            <TextField 
+                onChange={()=>{}}
+                placeholder="Search"
+                widthMode="number"
+                widthModeNumber={600}
+                leadingIcon="search"
+                trailingIcon="search"
+                trailingIconAction={()=>{}}
+            />
         </DivFlexColumn>
     )
 }
