39e804684b79eeb5eb52e2e9110436e312a4ce8c
Khuong Anh Kiet <teaz.khuonganhkiet@gmail.com>
2025-11-13 22:06:20 +0700

    placeholder for game screen

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+'use client'
+
+import { DivFlexColumn } from '@/components/LayoutDiv/LayoutDiv'
+import { TextBodyLarge, TextHeadlineLarge } from '@/components/TextBox/textBox'
+import { useTranslation } from 'react-i18next'
 
 export default function Game() {
+    const { t: t_common } = useTranslation('common')
+    const { t: t_toast } = useTranslation('toast')
+
     return (
-        <div>Game</div>
+        <DivFlexColumn style={{
+            gap: 'var(--Spacing-Spacing-M, 24px)',
+            padding: 'var(--Spacing-Spacing-M, 24px) var(--Spacing-Spacing-S, 16px)'
+        }}>
+            <TextHeadlineLarge children={t_common('game-page')} />
+            <TextBodyLarge>⚠️ {t_toast('info.inDevelopment')} ⚠️</TextBodyLarge>
+
+            <iframe src="https://nebezb.com/floppybird/" width="100%" height="500" className='CM-border-radius-mode-default' style={{border:'none'}}></iframe>
+
+        </DivFlexColumn>
+
     )
 }
